32c6cd6ae4b4d475f68de23d8b5fde08e2f78d12
Eric Thomas <eric.l.m.thomas@gmail.com>
2016-10-14 11:06:34 -0600

    flag matching

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,45 @@ module Main exposing (main)
 
 import Ports
 import Model exposing (Flags, Model, Msg(Start))
+import Regex exposing (contains, regex)
 import Update
 import Worker
+
+
+type Flag
+  = ArgumentFlag
+  | BooleanFlag
+  | ShortFlag
+  | PositionalFlag
+
+
+argumentRegex = regex "--(\\w|-)+=\\w+$" --- --num-threads=3
+booleanRegex = regex "--\\w+$"           --- --verbose
+shortRegex = regex "-\\w$"               --- -h
+
+flagMatcher : String -> Flag
+flagMatcher flag =
+    if contains argumentRegex flag then ArgumentFlag
+    else if contains booleanRegex flag then BooleanFlag
+    else if contains shortRegex   flag then ShortFlag
+    else PositionalFlag
+
+
+parseFlag : String -> String
+parseFlag flag =
+    case flagMatcher flag of
+      ArgumentFlag ->
+        "Argument flag"
+      BooleanFlag ->
+        "Boolean flag"
+      ShortFlag ->
+        "Short flag"
+      PositionalFlag ->
+        "Positional flag"
+
+parseArgs : List String -> List String
+parseArgs args =
+    List.map parseFlag args
 
 
 main : Program Flags
@@ -19,7 +56,7 @@ init : Flags -> ( Model, Cmd Msg )
 init flags =
     let
         model =
-            { args = flags.args
+            { args = parseArgs(flags.args)
             , createdAt = flags.createdAt
             }
 
